4aff460bdaa274c5390f627867534f1dade9b731
Lovelord88 <vivienneyahaya86@gmail.com>
2026-04-02 19:55:55 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from ECS Fargate!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening on port ${port}`);
+});
+
+
